43f167c310b81823a7a4d4bfffec07f817653859
Marcelo José Martins Pereira <marjopereira10@gmail.com>
2024-08-15 14:38:37 -0300

    :sparkles: feat: Finalização do projeto com funcionalidade de troca de cor

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const cores = ['azul', 'vermelho', 'verde', 'amarelo', 'roxo', 'laranja', 'rosa', 'preto', 'branco', 'cinza'];
+const cores = ['Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Purple', 'Orange', 'Brown', 'Gray', 'White'];
 const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() { 
+    const random = Math.floor(Math.random() * cores.length);
+    document.body.style.background = cores[random];
+    document.getElementById('cor').innerHTML = cores[random];
+}   );
